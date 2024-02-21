3e92afe637dce806a75a2cd3de7389c5d494abdc
Daniel Ramos <danielramos010903@gmail.com>
2024-02-21 18:43:21 -0500

    express

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, '../client')))
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`server has started on port ${process.env.PORT || 5000}`)
