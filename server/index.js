383c6e5a1a696d328a607f160e579c5041f01413
Daniel Ramos <danielramos010903@gmail.com>
2024-02-21 21:05:12 -0500

    po

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
-app.use(express.static(path.join(__dirname, '../client')))
+app.use(cors())
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/public/index.html'))
+    res.sendFile('../client/public/index.html')
 })
 
 app.listen(process.env.PORT || 5000, () => {
