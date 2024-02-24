27c4a554407a56326f822e39300c08b4e350a49d
Daniel Ramos <danielramos010903@gmail.com>
2024-02-23 23:20:23 -0500

    Fixed .env names

--- server/db.js
+++ server/db.js
@@ -3,11 +3,11 @@ require("dotenv").config({path: "../.env"});
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.LOCAL_DB_USER,
-  password: process.env.LOCAL_DB_PASSWORD,
-  host: process.env.LOCAL_DB_HOST,
-  port: process.env.LOCAL_DB_PORT,
-  database: process.env.LOCAL_DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
