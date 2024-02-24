dff048c59dda22773a186a804c97eb20eaecfd53
Daniel Ramos <danielramos010903@gmail.com>
2024-02-23 22:19:40 -0500

    Fixed .gitignore and Server-Side Routing

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../client/build/index.html"));
+});
+
 app.post("/register", async (req, res) => {
   try {
     const body = req.body;
