ab3d93c1548f1a05882da72f0f0fe8a688b13a2e
MarkMango21 <mark2asf@gmail.com>
2024-04-07 18:09:50 -0700

    New background feature for Events

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import React from 'react';
 import "../App.js";
+import MapOfUniversities from '../assets/Map-of-universities.png';
 const Events = ({userName}) => {
     {/*console.log('Received userName prop:', userName); // Log the received prop value to the console
     const [name,setName] = useState('mario');
@@ -11,8 +12,17 @@ const Events = ({userName}) => {
       setAge('30');
     }
   */}
+
+
     return (
-      <div>
+      <div style={{ backgroundImage: `url(${MapOfUniversities})`,
+        backgroundRepeat:"no-repeat",
+        backgroundSize:"auto", 
+        backgroundPosition:"center",
+        height: "100vh", // Set the height to 100% of viewport height
+        width: "100vw", // Set the width to 100% of viewport width
+        overflow: "hidden", // Hide any content that overflows the container     
+      }}>
         
         <h1>Welcome to Events!</h1>
         {/*<h2>Hello again {userName}!</h2>
