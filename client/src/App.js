7f4a298094b666185a9b969900679db241a3450a
Daniel Ramos <danielramos010903@gmail.com>
2024-04-07 23:11:41 -0400

    pls stop

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
