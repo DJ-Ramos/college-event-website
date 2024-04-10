00a5e1541d5d92d089ad1702aabd1296d9453adc
Daniel Ramos <122489501+DJ-Ramos@users.noreply.github.com>
2024-04-10 15:19:27 -0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  connectionString: "postgres://u3gfpdvq223n7i:p528f9fc0187cf76e0b8ba02a4364c2c5f4c69c753b68f3a89940d7334f294597@c7gljno857ucsl.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d748ho327flu5a"
+  connectionString: "postgres://u3gfpdvq223n7i:p528f9fc0187cf76e0b8ba02a4364c2c5f4c69c753b68f3a89940d7334f294597@c7gljno857ucsl.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d748ho327flu5a",
+  ssl: {rejectUnauthorized: false},
 });
 
 module.exports = pool;
